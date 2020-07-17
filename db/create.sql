521a72a0d04e9ed7dcc198ea01fea568097b9d77
Ol Gen <oggen18@gmail.com>
2020-07-17 09:57:03 +0300

    1. Sql схема для проекта Tracker. [#276987]

---
+++ db/create.sql
@@ -0,0 +1,4 @@
+--скрипт создания таблицы items
+create table items (
+id serial primary key,
+name text));
---
+++ db/insert.sql
@@ -0,0 +1,4 @@
+--скрипты вставок в таблицу items
+insert into items (name) values ('item1');
+insert into items (name) values ('item2');
+insert into items (name) values ('item3');
