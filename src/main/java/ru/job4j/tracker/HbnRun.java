86e0e335dca849fe3aaa3204cacad8189af520a4
Ol Gen <oggen18@gmail.com>
2021-03-08 19:45:10 +0300

    Added some fields to the model, and allowed Hibernate to automatically alter the table.

---
+++ src/main/java/ru/job4j/tracker/HbnRun.java
@@ -0,0 +1,36 @@
+package ru.job4j.tracker;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import ru.job4j.tracker.model.Item;
+
+import java.sql.Timestamp;
+import java.util.Date;
+
+public class HbnRun {
+    public static void main(String[] args) {
+        final StandardServiceRegistry registry = new StandardServiceRegistryBuilder()
+                .configure().build();
+        try {
+            SessionFactory sf = new MetadataSources(registry)
+                    .buildMetadata()
+                    .buildSessionFactory();
+            Session session = sf.openSession();
+            session.beginTransaction();
+            Item item = new Item(
+                    "New exteded item",
+                    "Added into the table by hibernate",
+                    new Timestamp(new Date().getTime()));
+            session.save(item);
+            session.getTransaction().commit();
+            session.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
+}
--- src/main/java/ru/job4j/tracker/model/Item.java
+++ src/main/java/ru/job4j/tracker/model/Item.java
@@ -1,6 +1,9 @@
 package ru.job4j.tracker.model;
 
 import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Objects;
 
 @Entity
 @Table(name = "items")
@@ -9,12 +12,20 @@ public class Item {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
+    private String description;
+    private Timestamp created;
 
     public Item() {
     }
 
     public Item(String name) {
         this.name = name;
+    }
+
+    public Item(String name, String description, Timestamp created) {
+        this.name = name;
+        this.description = description;
+        this.created = created;
     }
 
     public Integer getId() {
@@ -33,15 +44,41 @@ public class Item {
         this.name = name;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Timestamp getCreated() {
+        return created;
+    }
+
+    public void setCreated(Timestamp created) {
+        this.created = created;
+    }
+
     @Override
     public String toString() {
         return "Item{name='" + name + '\'' + ", id=" + id + '}';
     }
 
-//    @Override
-//    public int compareTo(Item another) {
-//        long first = Long.parseLong(id);
-//        long second = Long.parseLong(another.id);
-//        return Long.compare(first, second);
-//    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Item item = (Item) o;
+        return id == item.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
